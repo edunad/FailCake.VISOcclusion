8de5a11bad12746b3bbfcf4377a6701977c9ccaf
FailCake <4944278+edunad@users.noreply.github.com>
2025-11-17 17:28:59 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# FailCake.VISOcclusion
-VIS occlusion system
+<h2>
+	<p align="center">
+		FailCake.VISOcclusion
+	</p>
+</h2>
+<h4>
+	<p align="center">
+		<a href="/LICENSE"><img alt="logo" src="https://img.shields.io/github/license/edunad/FailCake.VISOcclusion"/>&nbsp;</a>
+		<a href="https://github.com/edunad/FailCake.VISOcclusion/issues?q=is%3Aopen+is%3Aissue+label%3ABUG"><img alt="logo" src="https://img.shields.io/github/issues/edunad/FailCake.VISOcclusion/BUG.svg"/>&nbsp;</a>
+		<a href="https://github.com/edunad/FailCake.VISOcclusion/commits/master/"><img alt="logo" src="https://img.shields.io/github/last-commit/edunad/FailCake.VISOcclusion.svg"/>&nbsp;</a><br/>
+		<br/>
+		<a href="#installation">Installation</a> -
+		<a href="#features">Features</a>
+	</p>
+</h4>
+
+> [!IMPORTANT]
+> Originally built for [Delivery & Beyond](https://store.steampowered.com/app/3376480/Delivery__Beyond/), not really intended for public use. But after thinking about it, figured I'd share it anyway. Some stuff is still hardcoded for the game, but eventually I want to turn this into a proper plugin.
+
+## Installation
+<img width="256" height="256" src="https://github.com/user-attachments/assets/5d3fcc53-d3b5-4938-94b6-9974f772c646" />
+
+Open Unity's Package Manager and add a new Git URL:
+```
+https://github.com/edunad/FailCake.VISOcclusion.git?path=/com.failcake.vis.occlusion
+```
+
+## Features
---
+++ com.failcake.vis.occlusion/Scripts/Renderer/VISFeature.cs
@@ -0,0 +1,147 @@
+﻿#region
+
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+
+#endregion
+
+namespace FailCake.VIS
+{
+    public class VISRendererFeature : ScriptableRendererFeature
+    {
+        public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingOpaques;
+
+        [Header("Settings"), Range(0, byte.MaxValue)]
+        public byte samples = 32;
+
+        [Range(-1, 1)]
+        public float padding = -0.02F;
+
+        #region PRIVATE
+
+        private VISRenderPass _visRenderPass;
+
+        #endregion
+
+        public override void Create() {
+            if (this._visRenderPass != null) return;
+
+            this._visRenderPass = new VISRenderPass(this.samples, this.padding) {
+                renderPassEvent = this.renderPassEvent
+            };
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData) {
+            if (!VISController.Instance || renderingData.cameraData.renderType != CameraRenderType.Base) return;
+
+            if (renderingData.cameraData.cameraType == CameraType.SceneView) return;
+            if (renderingData.cameraData.camera != Camera.main) return;
+
+            renderingData.cameraData.camera.depthTextureMode |= DepthTextureMode.Depth;
+            renderer.EnqueuePass(this._visRenderPass);
+        }
+
+        protected override void Dispose(bool disposing) {
+            if (!disposing) return;
+            this._visRenderPass = null;
+        }
+
+        private class VISRenderPass : ScriptableRenderPass
+        {
+            private readonly ComputeShader _computeShader;
+            private readonly int _kernelIndex;
+
+            private Vector4 _occlusionParams = new Vector4(
+                32, // Sample
+                0.01f,
+                0.02f, // Padding
+                0.0F // TotalPortals
+            );
+
+            public VISRenderPass(byte samples, float padding) {
+                this._occlusionParams.x = samples;
+                this._occlusionParams.z = padding;
+
+                this._computeShader = Resources.Load<ComputeShader>("Shaders/VISOcclusion");
+                if (!this._computeShader) throw new UnityException("Failed to load VISOcclusion compute shader from Resources/Shaders/VISOcclusion.compute!");
+
+                this._kernelIndex = this._computeShader.FindKernel("CullPortals");
+                if (this._kernelIndex < 0) throw new UnityException("Failed to find 'CullPortals' kernel in VISOcclusion compute shader!");
+            }
+
+            public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData) {
+                if (!this._computeShader || !VISController.Instance) return;
+
+                ComputeBuffer portalBuffer = VISController.Instance.GetPortalBuffer();
+                if (portalBuffer == null || !portalBuffer.IsValid() || portalBuffer.count == 0) return;
+
+                IReadOnlyList<entity_vis_portal> portals = VISController.Instance.GetPortals();
+                if (portals is not { Count: > 0 }) return;
+
+                UniversalResourceData universalResources = frameData.Get<UniversalResourceData>();
+                if (universalResources == null || !universalResources.cameraDepthTexture.IsValid()) return;
+
+                this._occlusionParams.w = portals.Count;
+
+                using (IComputeRenderGraphBuilder builder = renderGraph.AddComputePass("VIS PORTALS", out PassData passData))
+                {
+                    builder.AllowPassCulling(false);
+
+                    passData.occlusionShader = this._computeShader;
+                    passData.kernelIndex = this._kernelIndex;
+
+                    passData.portalBuffer = portalBuffer;
+                    passData.totalPortals = portals.Count;
+
+                    passData.depthTexture = universalResources.cameraDepthTexture;
+
+                    builder.UseTexture(passData.depthTexture);
+                    builder.SetRenderFunc((PassData data, ComputeGraphContext context) => this.ExecutePass(data, context));
+                }
+                // --------------
+            }
+
+            private void ExecutePass(PassData data, ComputeGraphContext context) {
+                ComputeCommandBuffer cmd = context.cmd;
+
+                int threadGroups = Mathf.CeilToInt(data.totalPortals / 64F);
+
+                // OCCLUSION ---
+                cmd.SetComputeVectorParam(data.occlusionShader, "_OcclusionParams", this._occlusionParams);
+                cmd.SetComputeBufferParam(data.occlusionShader, data.kernelIndex, "_PortalDataBuffer", data.portalBuffer);
+                cmd.SetComputeTextureParam(data.occlusionShader, data.kernelIndex, "_VISDepthTexture", data.depthTexture, 0, RenderTextureSubElement.Default);
+
+                cmd.DispatchCompute(data.occlusionShader, data.kernelIndex, threadGroups, 1, 1);
+                // --------------
+            }
+
+            private class PassData
+            {
+                internal ComputeShader occlusionShader;
+                internal int kernelIndex;
+
+                internal ComputeBuffer portalBuffer;
+                internal int totalPortals;
+
+                internal TextureHandle depthTexture;
+            }
+        }
+    }
+}
+
+/*# MIT License Copyright (c) 2025 FailCake
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
